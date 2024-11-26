0cd6baf4fed4d296b9f6db5c908ed2354c2d9e48
Abdul Moiz <moiza0799@gmail.com>
2024-11-26 14:44:21 -0600

    Implemented Feature Switch

--- DotNetFeatureImplementation/Program.cs
+++ DotNetFeatureImplementation/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+
+AppContext.SetSwitch("Feature.IsSupported", true);
+
+if (Feature.IsSupported)
+{
+    Feature.Implementation();
+}
+else Console.WriteLine("Feature is not supported");
+
+
+public static class Feature
+{
+    [FeatureSwitchDefinition("Feature.IsSupported")]
+    internal static bool IsSupported => AppContext.TryGetSwitch("Feature.IsSupported", out bool isEnable) ? isEnable : true;
+
+    internal static void Implementation()
+    {
+        Console.WriteLine("Feature is enable and running");
+    }
+}
