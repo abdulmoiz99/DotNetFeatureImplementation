231fe2634ce0f85c24b69b7ff3cd9f6552b24282
Abdul Moiz <moiza0799@gmail.com>
2024-11-26 17:53:06 -0600

    Induction Variable Widening

--- DotNetFeatureImplementation/Program.cs
+++ DotNetFeatureImplementation/Program.cs
@@ -1,11 +1,16 @@
-﻿using DotNetFeatureImplementation;
+﻿int[] array = { 1, 2, 3, 4, 5, 6 };
 
-GC.Collect();
-GC.WaitForPendingFinalizers();
-GC.Collect();
-long memoryBefore = GC.GetTotalMemory(false);
-var singleton = Singleton.Instance();
-long memoryAfter = GC.GetTotalMemory(false);
-Console.WriteLine($"Memory before: {memoryBefore} bytes");
-Console.WriteLine($"Memory after: {memoryAfter} bytes");
-Console.WriteLine($"Memory used by singleton: {memoryAfter - memoryBefore} bytes");
+
+Console.WriteLine(Sum(array));
+static int Sum(int[] array)
+{
+    int sum = 0;
+    //Previouly I was store are 32 bit integer // 4 bytes
+    for (int i = 0; i < array.Length; i++)
+    {
+        //When we use i, JIT uses zero-extenstion, in that we convert 32 bit to 64 bit or 4 Bytes to 8 Bytes
+        //Now we store int as 64 bit or 8 Byte, so we dont use zero extension any more, This is called Induction Variable (IV) windening
+        sum+= array[i];
+    }
+    return sum;
+}
