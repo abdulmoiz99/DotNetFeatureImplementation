f0ef029a10e0382ec153bf00bb9bcae94081515c
Abdul Moiz <moiza0799@gmail.com>
2024-11-27 07:38:15 -0600

    Tested Base64

--- DotNetFeatureImplementation/Program.cs
+++ DotNetFeatureImplementation/Program.cs
@@ -1,16 +1,15 @@
-﻿int[] array = { 1, 2, 3, 4, 5, 6 };
+﻿// Original data
+using System.Buffers.Text;
+using System.Text;
 
+string originalText = "Hello, Base64Url!";
+byte[] data = Encoding.UTF8.GetBytes(originalText);
 
-Console.WriteLine(Sum(array));
-static int Sum(int[] array)
-{
-    int sum = 0;
-    //Previouly I was store are 32 bit integer // 4 bytes
-    for (int i = 0; i < array.Length; i++)
-    {
-        //When we use i, JIT uses zero-extenstion, in that we convert 32 bit to 64 bit or 4 Bytes to 8 Bytes
-        //Now we store int as 64 bit or 8 Byte, so we dont use zero extension any more, This is called Induction Variable (IV) windening
-        sum+= array[i];
-    }
-    return sum;
-}
+// Encoding to Base64Url
+string encoded = Base64Url.EncodeToString(data);
+Console.WriteLine($"Encoded: {encoded}");
+
+// Decoding back to original data
+byte[] decodedData = Base64Url.DecodeFromChars(encoded);
+string decodedText = Encoding.UTF8.GetString(decodedData);
+Console.WriteLine($"Decoded: {decodedText}");
