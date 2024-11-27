7ad5e564b031b678091bd43f487ff7d9aefcdd4d
Abdul Moiz <moiza0799@gmail.com>
2024-11-27 14:55:12 -0600

    GetAlternateLookup

--- DotNetFeatureImplementation/Program.cs
+++ DotNetFeatureImplementation/Program.cs
@@ -1,10 +1,22 @@
-﻿var value = new Coords();
-Console.WriteLine(value.x);
-Console.WriteLine(value.y);
+﻿var stringPara = "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph";
 
+var result = CountWords(stringPara);
 
-public readonly struct Coords
+foreach (var value in result)
 {
-    public int x { get; init; }
-    public int y { get; init; }
+    Console.WriteLine("{0} - {1}", value.Key, value.Value);
+}
+
+static Dictionary<string, int> CountWords(ReadOnlySpan<char> para)
+{
+    Dictionary<string, int> wordCounts = new(StringComparer.OrdinalIgnoreCase);
+    Dictionary<string, int>.AlternateLookup<ReadOnlySpan<char>> spanLookUp =
+        wordCounts.GetAlternateLookup<ReadOnlySpan<char>>();
+
+    foreach (Range wordRange in para.Split(' '))
+    {
+        ReadOnlySpan<char> word = para[wordRange];
+        spanLookUp[word] = spanLookUp.TryGetValue(word, out int count) ? count + 1 : 1;
+    }
+    return wordCounts;
 }
