4d5da0fab651f964eea8279e615b92a81e6df7ce
Abdul Moiz <moiza0799@gmail.com>
2024-11-27 14:39:07 -0600

    findings related to struck

--- DotNetFeatureImplementation/Program.cs
+++ DotNetFeatureImplementation/Program.cs
@@ -1,40 +1,10 @@
-﻿
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-
-BenchmarkRunner.Run<Benchy>();
+﻿var value = new Coords();
+Console.WriteLine(value.x);
+Console.WriteLine(value.y);
 
 
-[MemoryDiagnoser]
-public class Benchy
+public readonly struct Coords
 {
-    private static readonly string _dataAsText = "11 27 2024";
-
-    [Benchmark]
-    public (int month, int day, int year) DateWithStringAndSubstring()
-    {
-        var monthAsText = _dataAsText.Substring(0, 2);
-        var dayAsText = _dataAsText.Substring(3, 2);
-        var yearAsText = _dataAsText.Substring(6);
-
-        int month = int.Parse(monthAsText);
-        int day = int.Parse(dayAsText);
-        int year = int.Parse(yearAsText);
-
-        return (month, day, year);
-    }
-    [Benchmark]
-    public (int month, int day, int year) DateWithStringAndSpan()
-    {
-        ReadOnlySpan<char> dateAsSpan = _dataAsText;
-        var monthAsText = dateAsSpan.Slice(0, 2);
-        var dayAsText = dateAsSpan.Slice(3, 2);
-        var yearAsText = dateAsSpan.Slice(6);
-
-        int month = int.Parse(monthAsText);
-        int day = int.Parse(dayAsText);
-        int year = int.Parse(yearAsText);
-
-        return (month, day, year);
-    }
+    public int x { get; init; }
+    public int y { get; init; }
 }
