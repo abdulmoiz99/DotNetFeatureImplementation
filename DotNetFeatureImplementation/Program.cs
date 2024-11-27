425600a0ee6e1a4aa6af473887bab532f01f7e22
Abdul Moiz <moiza0799@gmail.com>
2024-11-27 14:58:57 -0600

    OrderedDictionary

--- DotNetFeatureImplementation/Program.cs
+++ DotNetFeatureImplementation/Program.cs
@@ -1,22 +1,18 @@
-﻿var stringPara = "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph";
+﻿var dictionay = new OrderedDictionary<int, string>();
 
-var result = CountWords(stringPara);
+dictionay.Add(3, "Test3");
+dictionay.Add(1, "Test1");
+dictionay.Add(2, "Test2");
+dictionay.Add(4, "Test4");
 
-foreach (var value in result)
+foreach(var item in dictionay)
 {
-    Console.WriteLine("{0} - {1}", value.Key, value.Value);
+    Console.WriteLine("{0}--{1}", item.Key, item.Value);
 }
 
-static Dictionary<string, int> CountWords(ReadOnlySpan<char> para)
-{
-    Dictionary<string, int> wordCounts = new(StringComparer.OrdinalIgnoreCase);
-    Dictionary<string, int>.AlternateLookup<ReadOnlySpan<char>> spanLookUp =
-        wordCounts.GetAlternateLookup<ReadOnlySpan<char>>();
+dictionay.RemoveAt(0);
 
-    foreach (Range wordRange in para.Split(' '))
-    {
-        ReadOnlySpan<char> word = para[wordRange];
-        spanLookUp[word] = spanLookUp.TryGetValue(word, out int count) ? count + 1 : 1;
-    }
-    return wordCounts;
+foreach (var item in dictionay)
+{
+    Console.WriteLine("{0}--{1}", item.Key, item.Value);
 }
